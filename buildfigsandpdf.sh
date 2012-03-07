55853dbe590b33ee248eb58af7a7279686629ac9
Robby McKilliam <robby.mckilliam@gmail.com>
2012-03-08 10:21:19 +1030

    Fixed more annoying dos line endings.

--- buildfigsandpdf.sh
+++ buildfigsandpdf.sh
@@ -3,17 +3,17 @@ mpost -interaction=nonstopmode circnomeansym.mp
 mpost -interaction=nonstopmode rotatedpic.mp 
 mpost -interaction=nonstopmode wrappeduniform_var0p08.mp 
 mpost -interaction=nonstopmode VonMisesSumUnif_0p3.mp 
-cd ..
+cd .. 
 
 cd plots 
-mpost -interaction=nonstopmode directionestplot.mp
-mpost -interaction=nonstopmode delayestplot.mp
-mpost -interaction=nonstopmode sumdistplot.mp
-cd ..
+mpost -interaction=nonstopmode directionestplot.mp 
+mpost -interaction=nonstopmode delayestplot.mp 
+mpost -interaction=nonstopmode sumdistplot.mp 
+cd .. 
 
-latex papersm.tex
-bibtex papersm
-latex papersm.tex
-latex papersm.tex
-dvips papersm.dvi
-ps2pdf papersm.ps
+latex papersm.tex 
+bibtex papersm 
+latex papersm.tex 
+latex papersm.tex 
+dvips papersm.dvi 
+ps2pdf papersm.ps 
