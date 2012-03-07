0a6f8ff5bd5c0b2109d854030ddb80aafd51f438
Robby McKilliam <robby.mckilliam@gmail.com>
2012-03-08 09:46:28 +1030

    minor edits to buildfigs.pdf

--- buildfigsandpdf.sh
+++ buildfigsandpdf.sh
@@ -1,11 +1,11 @@
-cd figs
-mpost -interaction=nonstopmode circnomeansym.mp
-mpost -interaction=nonstopmode rotatedpic.mp
-mpost -interaction=nonstopmode wrappeduniform_var0p08.mp
-mpost -interaction=nonstopmode VonMisesSumUnif_0p3.mp
+cd figs 
+mpost -interaction=nonstopmode circnomeansym.mp 
+mpost -interaction=nonstopmode rotatedpic.mp 
+mpost -interaction=nonstopmode wrappeduniform_var0p08.mp 
+mpost -interaction=nonstopmode VonMisesSumUnif_0p3.mp 
 cd ..
 
-cd plots
+cd plots 
 mpost -interaction=nonstopmode directionestplot.mp
 mpost -interaction=nonstopmode delayestplot.mp
 mpost -interaction=nonstopmode sumdistplot.mp
