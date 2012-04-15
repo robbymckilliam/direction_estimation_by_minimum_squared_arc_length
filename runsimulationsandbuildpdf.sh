812233ac9997f1d077c10fec27ac0872802d75d7
Robby McKilliam <robby.mckilliam@gmail.com>
2012-04-15 15:05:14 +0930

    fixed silly merge bug

--- runsimulationsandbuildpdf.sh
+++ runsimulationsandbuildpdf.sh
@@ -1,20 +1,20 @@
-cd figs
-mpost -interaction=nonstopmode circnomeansym.mp
-mpost -interaction=nonstopmode rotatedpic.mp
-mpost -interaction=nonstopmode wrappeduniform_var0p08.mp
-mpost -interaction=nonstopmode VonMisesSumUnif_0p3.mp
-cd ..
+cd figs 
+mpost -interaction=nonstopmode circnomeansym.mp 
+mpost -interaction=nonstopmode rotatedpic.mp 
+mpost -interaction=nonstopmode wrappeduniform_var0p08.mp 
+mpost -interaction=nonstopmode VonMisesSumUnif_0p3.mp 
+cd .. 
 
-cd plots
-scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar bearingsim.scala
-scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar clt.scala
-mpost -interaction=nonstopmode directionestplot.mp
-mpost -interaction=nonstopmode delayestplot.mp
-mpost -interaction=nonstopmode sumdistplot.mp
-cd ..
+cd plots 
+scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar bearingsim.scala 
+scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar clt.scala 
+mpost -interaction=nonstopmode directionestplot.mp 
+mpost -interaction=nonstopmode delayestplot.mp 
+mpost -interaction=nonstopmode sumdistplot.mp 
+cd .. 
 
-latex papersm.tex
-bibtex papersm
+latex papersm.tex 
+bibtex papersm 
 latex papersm.tex
 latex papersm.tex
 dvips papersm.ps
